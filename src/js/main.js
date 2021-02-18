7438dab6456b3986cf8fb65c1b5fcc563f43cf97
Arif Ullah <aullah1@hotmail.co.uk>
2021-02-18 15:57:48 +0000

    Add JS smooth scrolling to anchor links.

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,19 @@ import QuickMenu from "quickmenu";
 
 new QuickMenu();
 
-// console.log('Hello');
+// Smooth scroll on anchor links.
+document.querySelectorAll('a').forEach(item => {
+    item.addEventListener('click', function (e) { // Listen to all hyperlinks.
+        let href = e.target.getAttribute('href');
+        if (href.charAt(0) == '#') { // If anchor link.
+            e.preventDefault();
+            if (href.length == 1) { // Go to top.
+                window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+            } else {
+                document.querySelector(href).scrollIntoView({
+                  behavior: 'smooth'
+                });
+            }
+        }
+    })
+});
