189d5761c788a4bea5fb008a3bf63e88f1256061
Arif Ullah <aullah1@hotmail.co.uk>
2021-02-18 20:26:59 +0000

    Do not extract comments on compiled code.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
     entry: './src/js/main.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist/js'),
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+            }),
+        ],
     },
 };
